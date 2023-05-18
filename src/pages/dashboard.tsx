9c5f9a94ba478d1470be6102102d183ab00db70f
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-05-18 15:05:16 -0300

    Criando Header

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/Header";
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Dashboard;
