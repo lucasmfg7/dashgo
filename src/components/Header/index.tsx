cf88569a07926ceb5c8605e8062a2c8a7c07567e
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-05-20 16:03:23 -0300

    Sidebar responsiva

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
 import { Profile } from "./Profile";
 import { NotificationsNav } from "./NotificationsNav";
 import { SearchBox } from "./SearchBox";
 import { Logo } from "./Logo";
+import { useSidebarDrawer } from "@/contexts/SidebarDrawerContext";
+import { RiMenuLine } from "react-icons/ri";
 
 export const Header = () => {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +25,17 @@ export const Header = () => {
       mt="4"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          aria-label="open navigation"
+          mr="2"
+          mt="2"
+        ></IconButton>
+      )}
       <Logo />
       {isWideVersion && <SearchBox />}
       <Flex align="center" ml="auto">
