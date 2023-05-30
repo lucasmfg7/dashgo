b51f364c3b1cc076e614535eb9bc4cd536ba7d97
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-05-30 11:08:17 -0300

    Sinalizando link ativo

--- src/components/Sidebar/NavLink/index.tsx
+++ src/components/Sidebar/NavLink/index.tsx
@@ -1,6 +1,6 @@
 import { Icon, Link as ChakraLink, LinkProps, Text } from "@chakra-ui/react";
 import { ElementType } from "react";
-import Link from "next/link";
+import { ActiveLink } from "@/components/ActiveLink";
 
 interface NavLinkProps extends LinkProps {
   icon: ElementType;
@@ -10,13 +10,13 @@ interface NavLinkProps extends LinkProps {
 
 export const NavLink = ({ children, icon, href, ...rest }: NavLinkProps) => {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" alignItems="center" py="1" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">
           {children}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 };
