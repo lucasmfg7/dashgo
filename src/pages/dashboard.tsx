041f6a323444022189d2fd97a728849b770ba898
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-05-30 10:28:43 -0300

    Dashboard responsivo

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -72,14 +72,14 @@ const Dashboard = () => {
           minChildWidth="320px"
           alignItems="flex-start"
         >
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8}>
             <Text fontSize="lg" mb="4">
               Inscrits da semana
             </Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
 
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8}>
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
