2809a24754a6218bbb4103ce6c5a9c671d6b79ca
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-06-13 14:38:46 -0300

    Criando hook de listagem

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,7 +1,7 @@
 import { Header } from "@/components/Header";
 import { Pagination } from "@/components/Pagination";
 import { Sidebar } from "@/components/Sidebar";
-import { api } from "@/services/api";
+import { useUsers } from "@/services/hooks/useUsers";
 import {
   Box,
   Button,
@@ -21,27 +21,9 @@ import {
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from "react-query";
 
 const UserList = () => {
-  const { data, isLoading, isFetching, error } = useQuery("users", async () => {
-    const { data } = await api.get("users");
-
-    const users = data.users.map((user: any) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
-
-    return users;
-  });
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -96,7 +78,7 @@ const UserList = () => {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user: any) => (
+                  {data?.map((user) => (
                     <Tr key={user.id}>
                       <Td px={["4", "4", "6"]}>
                         <Checkbox colorScheme="pink" />
